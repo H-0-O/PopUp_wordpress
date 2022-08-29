9563cd604e765ea17dfb85c7cbddbb4d4868170f
hossein <ho.off.acc@gmail.com>
2022-08-29 18:48:17 +0430

    half of client_page

--- Client_Page.php
+++ Client_Page.php
@@ -3,8 +3,16 @@ class Client_Page
 {
     public function __construct()
     {
-
+        add_action('wp_enqueue_scripts' , [$this , 'add_files']);
     }
 
+    public function add_files()
+    {
+        wp_enqueue_style( 'bootstrap' , 'https://lib.arvancloud.com/bootstrap/5.1.3/css/bootstrap.min.css' , false , false ,'all');
+        wp_enqueue_script('client_side' , plugin_dir_url(__FILE__).'/assets/js/client-side.js');
+    }
+    private function generate_html()
+    {
 
+    }
 }
